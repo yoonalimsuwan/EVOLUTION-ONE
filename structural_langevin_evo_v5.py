773ca53b996818a1a0945fac4f0d184e36a18748
Yoon A Limsuwan (美美) <msps4u@gmail.com>
2026-07-12 13:05:43 +0700

    Update developer information in comments

--- structural_langevin_evo_v5.py
+++ structural_langevin_evo_v5.py
@@ -1,7 +1,7 @@
 # =============================================================================
 # LANGEVIN ADVANCED WITH STRUCTURAL CALCULUS  (v5)
 # =============================================================================
-# Developer  : Yoon A Limsuwan / MSPS NETWORK
+# Developer  : PAI , Yoon A Limsuwan / MSPS NETWORK
 # ORCID      : 0009-0008-2374-0788
 # GitHub     : yoonalimsuwan
 # License    : MIT
